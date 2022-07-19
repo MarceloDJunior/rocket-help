7d5abedb0998ce129bc6435dfef14170294f861e
Marcelo Dornelles Junior <marcelo_dj28@hotmail.com>
2022-07-19 20:41:22 -0300

    chore: adjust register screen layout

--- src/screens/register.tsx
+++ src/screens/register.tsx
@@ -6,17 +6,19 @@ import { Button } from '@/components/button';
 
 export const Register = () => {
   return (
-    <VStack flex={1} p={6} bg="gray.600">
+    <VStack flex={1} bg="gray.600">
       <Header title="Nova solicitação" />
-      <Input placeholder="Número do patrimônio" mt={4} />
-      <Input
-        placeholder="Descrição do problema"
-        mt={5}
-        flex={1}
-        multiline
-        textAlignVertical="top"
-      />
-      <Button title="Cadastrar" mt={5} />
+      <VStack flex={1} px={6} pb={6}>
+        <Input placeholder="Número do patrimônio" />
+        <Input
+          placeholder="Descrição do problema"
+          mt={5}
+          flex={1}
+          multiline
+          textAlignVertical="top"
+        />
+        <Button title="Cadastrar" mt={5} />
+      </VStack>
     </VStack>
   );
 };
